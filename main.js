ceb224cae42ee1b2b7405666684d7448d82b7c5b
Thomas <malifarge.thomas9@gmail.com>
2022-07-04 18:30:53 +0200

    duel html + css and start js

--- main.js
+++ main.js
@@ -35,3 +35,81 @@ const Français = () =>{
         element.classList.remove("affich")
     });
 }
+
+// chiffre random
+
+const random = () => {
+    let result = ""
+    const numbers = Math.floor(Math.random()*150)+1
+    if (numbers <=50){
+        result = "pierre"
+    } else if (numbers >100){
+        result = "ciseaux"
+    } else {
+        result = "feuille"
+    }
+
+    return result, numbers
+
+
+}
+
+// preparation du jeu
+
+let scorePlayer = 0
+let scoreIA=0
+
+const game = () =>{
+    const card = document.getElementsByClassName("affich2")
+    card.classList.add("Naffich")
+    card.classList.remove("affich2")
+}
+
+// roulement de carte
+
+const cardmove = (start,j) =>{
+    iachoice.remove(`${start}`)
+    const arrayresutl = ["feuille","pierre","ciseaux"]
+    start = arrayresutl[i]
+    j++
+    return start
+}
+
+// si le choix est ciseaux 
+
+const choiceScissors = () =>{
+    const cardChoice = document.getElementById("resulta")
+    const iachoice = document.getElementsById("IA")
+    game()
+    cardChoice.classList.add("affich2")
+    cardChoice.classList.add("ciseaux")
+    cardChoice.classList.remove("Naffich")
+    random()
+    if (result ==="feuille"){
+        iachoice.classList.remove("random")
+        for (let i=0; i<25;i++){
+            cardmove("feuille", 0)
+            cardChoice.classList.add(`${start}`)
+            setInterval(cardmove,1000)
+        }
+        iachoice.classList.add("feuille")
+        scorePlayer ++
+    } else if (result === "pierre"){
+        iachoice.classList.remove("random")
+        for (let i=0; i<25;i++){
+            cardmove("feuille", 0)
+            cardChoice.classList.add(`${start}`)
+            setInterval(cardmove,1000)
+        }
+        iachoice.classList.add("pierre")
+        scoreIA ++
+    } else {
+        iachoice.classList.remove("random")
+        for (let i=0; i<25;i++){
+            cardmove("feuille", 0)
+            cardChoice.classList.add(`${start}`)
+            setInterval(cardmove,1000)
+        }
+        iachoice.classList.add("ciseaux")
+    }
+}
